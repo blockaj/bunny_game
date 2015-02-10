133a751d1ca5b66beff44f3ed78788f22b36749d
Aaron <ajblock@gmail.com>
2015-02-10 09:42:46 -0500

    Amazing functiony main file

--- main.go
+++ main.go
@@ -5,4 +5,36 @@ import (
 )
 
 func main() {
+
+}
+
+//Creates a certain number of bunnies in linked-list
+func createBunnies(root *Bunny, numberOfBunnies int) Bunny {
+	root = NewBunny()
+	ranger := &Bunny{}
+	ranger = root // Will move through list
+
+	for i := 0; i < numberOfBunnies - 1; i++ { 
+		bunny := NewBunny()
+		ranger.Next = bunny
+		ranger = ranger.Next
+		ranger.Next = nil
+	}
+	return *root
+}
+
+//Prints entire list of bunnies given root of linked-list
+func printList(root *Bunny) {
+	ranger := &Bunny{}
+	ranger = root
+	if ranger != nil {
+		for ranger.Next != nil {
+
+		}
+	}
+}
+
+//Prints single bunny; used in func printList() for formatting purposes
+func printBunny(inputBunny Bunny) {
+
 }
