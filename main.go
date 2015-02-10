f52d61b9bff2721fc3be98b8370b02b36fcaf1d2
Aaron <ajblock@gmail.com>
2015-02-10 09:47:59 -0500

    Creates five bunnies, prints them

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-
+	root := createBunnies(5)
+	printList(root)
 }
 
 //Creates a certain number of bunnies in linked-list
-func createBunnies(root *Bunny, numberOfBunnies int) Bunny {
-	root = NewBunny()
+func createBunnies(numberOfBunnies int) *Bunny {
+	root := NewBunny()
 	ranger := &Bunny{}
 	ranger = root // Will move through list
 
@@ -20,7 +21,7 @@ func createBunnies(root *Bunny, numberOfBunnies int) Bunny {
 		ranger = ranger.Next
 		ranger.Next = nil
 	}
-	return *root
+	return root
 }
 
 //Prints entire list of bunnies given root of linked-list
@@ -29,12 +30,14 @@ func printList(root *Bunny) {
 	ranger = root
 	if ranger != nil {
 		for ranger.Next != nil {
-
+			printBunny(ranger)
+			ranger = ranger.Next
 		}
+		printBunny(ranger)
 	}
 }
 
 //Prints single bunny; used in func printList() for formatting purposes
-func printBunny(inputBunny Bunny) {
-
+func printBunny(inputBunny *Bunny) {
+	fmt.Println(inputBunny.Name(), "is a", inputBunny.Age(), "year(s) old", inputBunny.Gender().String(), "bunny with", inputBunny.Color().String(), "fur.")
 }
